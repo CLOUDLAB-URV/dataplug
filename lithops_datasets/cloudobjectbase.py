1d1fcc30e608e4d427f8d158c6df1e6de67df270
Aitor <aitor.a98@gmail.com>
2022-07-18 14:41:37 +0200

    Fix gzip preprocessing

--- lithops_datasets/cloudobjectbase.py
+++ lithops_datasets/cloudobjectbase.py
@@ -3,7 +3,11 @@ import re
 import logging
 import shutil
 
+import boto3
+import botocore
 import s3fs
+
+from typing import Tuple, Dict, BinaryIO
 
 key_regex = re.compile(r'^\w+://.+/.+$')
 
@@ -14,7 +18,7 @@ class CloudObjectBase:
     def __init__(self, cloud_object):
         self.cloud_object = cloud_object
 
-    def preprocess(self):
+    def preprocess(self, object_stream: BinaryIO) -> Tuple[bytes, Dict[str, str]]:
         raise NotImplementedError()
 
 
@@ -33,12 +37,19 @@ class CloudObject:
             key=s3_config.get('aws_access_key_id'),
             secret=s3_config.get('aws_secret_access_key'),
             client_kwargs=s3_config.get('s3_client_kwargs', None),
-            config_kwargs=s3_config.get('s3_config_kwargs', None),
+            config_kwargs=s3_config.get('s3_config_kwargs', None)
         )
 
+        self.s3_client = boto3.client('s3', aws_access_key_id=s3_config.get('aws_access_key_id'),
+                                      aws_secret_access_key=s3_config.get('aws_secret_access_key'),
+                                      region_name=s3_config.get('s3_client_kwargs', {}).get('region_name'),
+                                      endpoint_url=s3_config.get('s3_client_kwargs', {}).get('endpoint_url'),
+                                      config=botocore.client.Config(**s3_config.get('s3_config_kwargs', {})))
+
         self._bucket, self._key, self._version = self.s3.split_path(s3_path)
-        self._full_path = os.path.join(self._bucket, self._key)
-        self._full_meta_path = self._full_path + '.meta'
+        self._meta_key = self._key + '.meta'
+        self._full_key = os.path.join(self._bucket, self._key)
+        self._full_meta_key = self._full_key + '.meta'
 
         logger.debug(f'{self._bucket=},{self._key=},{self._version=}')
 
@@ -46,30 +57,23 @@ class CloudObject:
 
     @property
     def path(self):
-        return self._full_path
+        return self._full_key
 
     @classmethod
     def new_from_s3(cls, cloud_object_class, s3_path, s3_config=None):
         co_instance = cls(cloud_object_class, s3_path, s3_config)
-        return co_instance._child
+        return co_instance
 
-    def exists(self):
-        if not self._meta:
-            self.fetch()
-        return bool(self._meta)
+    @classmethod
+    def new_from_file(cls, cloud_object_class, file, cloud_path=None, s3_config=None):
+        if cloud_path is None and isinstance(file, str):
+            cloud_path = 's3://' + file
+        elif cloud_path is None:
+            raise Exception('Cloud path is required')
 
-    def is_staged(self):
-        return self.s3.exists(self._full_meta_path)
+        co_instance = cls(cloud_object_class, cloud_path, s3_config)
 
-    def fetch(self):
-        if not self._meta:
-            logger.debug('fetching object')
-            self._meta = self.s3.info(self._full_path)
-            logger.debug(self._meta)
-        return self._meta
-
-    def new_from_file(self, file):
-        if self.exists():
+        if co_instance.exists():
             raise Exception('Object already exists')
 
         if isinstance(file, str):
@@ -77,5 +81,30 @@ class CloudObject:
         else:
             stream = file
 
-        with self.s3.open(self._full_path, 'wb') as f:
+        with co_instance.s3.open(co_instance._full_key, 'wb') as f:
             shutil.copyfileobj(stream, f)
+
+    def exists(self):
+        if not self._meta:
+            self.fetch()
+        return bool(self._meta)
+
+    def is_staged(self):
+        return self.s3.exists(self._full_meta_key)
+
+    def fetch(self):
+        if not self._meta:
+            logger.debug('fetching object')
+            self._meta = self.s3.info(self._full_key)
+            logger.debug(self._meta)
+        return self._meta
+
+    def preprocess(self):
+        with self.s3.open(self._full_key) as input_stream:
+            body, meta = self._child.preprocess(input_stream)
+        self.s3_client.put_object(
+            Body=body,
+            Bucket=self._bucket,
+            Key=self._meta_key,
+            Metadata=meta
+        )
--- lithops_datasets/compressed/gzipped.py
+++ lithops_datasets/compressed/gzipped.py
@@ -1,43 +1,77 @@
 import logging
 import os
+import re
+import io
+import subprocess
+import tempfile
+from typing import BinaryIO
+
+import pandas as pd
+
 from ..cloudobjectbase import CloudObjectBase
 
-import subprocess
 
 logger = logging.getLogger(__name__)
 
+GZTOOL_PATH = '/home/lab144/.local/bin/gztool'
+CHUNK_SIZE = 65536
 
-class GZippedBlob(CloudObjectBase):
+RE_WINDOWS = re.compile(r'#\d+: @ \d+ / \d+ L\d+ \( \d+ @\d+ \)')
+RE_NUMS = re.compile(r'\d+')
+RE_NLINES = re.compile(r'Number of lines\s+:\s+\d+')
+
+
+class GZippedText(CloudObjectBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def preprocess(self):
-        with self.cloud_object.s3.open(self.cloud_object.path, 'rb') as co:
-            r, w = os.pipe()
+    def preprocess(self, object_stream: BinaryIO):
+        r, w = os.pipe()
 
-            proc = subprocess.Popen(['/home/lab144/.local/bin/gztool', '-i', '-x', '-s', '10'],
-                                    stdin=r)
-            pipe = os.fdopen(w, 'wb')
+        tmp_file_name = tempfile.mktemp()
+        # try:
+        #     os.remove(tmp_file_name)
+        # except FileNotFoundError:
+        #     pass
+        index_proc = subprocess.Popen([GZTOOL_PATH, '-i', '-x', '-s', '10', '-I', tmp_file_name],
+                                      stdin=r, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        pipe = os.fdopen(w, 'wb')
 
-            chunk = co.read(65536)
-            while chunk != b"":
-                pipe.write(chunk)
-                chunk = co.read(65536)
+        chunk = object_stream.read(CHUNK_SIZE)
+        while chunk != b"":
+            pipe.write(chunk)
+            chunk = object_stream.read(CHUNK_SIZE)
+        pipe.flush()
+        pipe.close()
+        object_stream.close()
 
-            stdout, stderr = proc.communicate()
-            assert stderr == b""
+        stdout, stderr = index_proc.communicate()
+        logger.debug(stdout.decode('utf-8'))
+        logger.debug(stderr.decode('utf-8'))
+        assert index_proc.returncode == 0
 
-        # TODO
-        # add creation of text index file
-        # upload both index file and text index file to object storage
+        with open(tmp_file_name, 'rb') as index_f:
+            output = subprocess.check_output([GZTOOL_PATH, '-ell'], input=index_f.read()).decode('utf-8')
+            logger.debug(output)
+        os.remove(tmp_file_name)
+
+        total_lines = RE_NUMS.findall(RE_NLINES.findall(output).pop()).pop()
+
+        lines = []
+        for f in RE_WINDOWS.finditer(output):
+            nums = [int(n) for n in RE_NUMS.findall(f.group())]
+            lines.append(nums)
+
+        df = pd.DataFrame(lines, columns=['window', 'compressed_byte', 'uncompressed_byte',
+                                          'line_number', 'window_size', 'window_offset'])
+        df.set_index(['window'], inplace=True)
+
+        out_stream = io.BytesIO()
+        df.to_parquet(out_stream, engine='pyarrow')
+        return out_stream.getvalue(), {'total_lines': total_lines}
 
     def partition_even_lines(self, lines_per_chunk):
         # TODO
         # make use of index to get the ranges of the chunks partitioned by number of lines per chunk
         # return list of range tuples [(chunk0-range0, chunk-0range1), ...]
         pass
-
-
-class GZippedText(GZippedBlob):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
