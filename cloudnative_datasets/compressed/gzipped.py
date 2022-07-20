e0e5ef6c6bed67d5b1b8f16b18e1187999d2f0af
Aitor <aitor.a98@gmail.com>
2022-07-20 17:42:02 +0200

    Fix partition chunk lines (wip)

--- cloudnative_datasets/compressed/gzipped.py
+++ cloudnative_datasets/compressed/gzipped.py
@@ -4,6 +4,7 @@ import re
 import io
 import subprocess
 import tempfile
+from math import ceil
 from typing import BinaryIO
 
 import pandas as pd
@@ -52,8 +53,8 @@ class GZippedText(CloudObjectBase):
             index_bin = index_f.read()
         output = subprocess.check_output([GZTOOL_PATH, '-ell'], input=index_bin).decode('utf-8')
         logger.debug(output)
-        self.cloud_object.s3_client.put_object(Bucket=self.cloud_object._meta_bucket, Key=self._index_key,
-                                               Body=index_bin)
+        self.cloud_object.s3.put_object(Bucket=self.cloud_object.meta_bucket, Key=self._index_key,
+                                        Body=index_bin)
 
         total_lines = RE_NUMS.findall(RE_NLINES.findall(output).pop()).pop()
 
@@ -71,18 +72,33 @@ class GZippedText(CloudObjectBase):
         df.to_csv('test.csv')
         return out_stream.getvalue(), {'total_lines': total_lines}
 
-    def partition_chunk_lines(self, lines_per_chunk):
+    def partition_chunk_lines(self, lines_per_chunk, strategy='expand'):
+        total_lines = int(self.cloud_object.get_attribute('total_lines'))
+        parts = ceil(total_lines / lines_per_chunk)
+        pairs = [(lines_per_chunk * i, (lines_per_chunk * i) + lines_per_chunk - 1) for i in range(parts)]
+
+        # adjust last pair
+        if pairs[-1][1] > total_lines:
+            if strategy == 'expand':
+                l0, _ = pairs[-1]
+                pairs[-1] = (l0, total_lines)
+            elif strategy == 'merge':
+                l0, l1 = pairs.pop()
+                extra = l1 - l0
+                pair = pairs[-1]
+                pairs[-1] = pair[0], pair[1] + extra
+            else:
+                raise Exception(f'Unknown strategy {strategy}')
+
+
+
+        return pairs
+
+    def partition_num_chunks(self, n_chunks):
         x = self.cloud_object.get_attribute('total_lines')
-        # TODO
-        # make use of index to get the ranges of the chunks partitioned by number of lines per chunk
-        # return list of range tuples [(chunk0-range0, chunk-0range1), ...]
         pass
 
-    def partition_n_chunks(self, n_chunks):
-        x = self.cloud_object.get_attribute('total_lines')
-        pass
-
-    def get_line_range(self, line0, line1):
+    def __get_line_range(self, line0, line1):
         meta_obj = self.cloud_object.get_meta_obj()
         meta_buff = io.BytesIO(meta_obj.read())
         meta_buff.seek(0)
@@ -110,12 +126,12 @@ class GZipLineIterator(CloudObjectChunk):
         tmp_index_file = tempfile.mktemp()
 
         # Get index and store it to temp file
-        res = self.cloud_object.s3_client.get_object(Bucket=self.cloud_object._meta_bucket, Key=self.child._index_key)
+        res = self.cloud_object._s3.get_object(Bucket=self.cloud_object._meta_bucket, Key=self.child._index_key)
         with open(tmp_index_file, 'wb') as index_tmp:
             index_tmp.write(res['Body'].read())
 
-        res = self.cloud_object.s3_client.get_object(Bucket=self.cloud_object._obj_bucket, Key=self.cloud_object._key,
-                                                     Range=f'bytes={self.range0 - 1}-{self.range1 - 1}')
+        res = self.cloud_object._s3.get_object(Bucket=self.cloud_object._obj_bucket, Key=self.cloud_object._key,
+                                               Range=f'bytes={self.range0 - 1}-{self.range1 - 1}')
         self._body = res['Body']
 
         tmp_chunk_file = tempfile.mktemp()
