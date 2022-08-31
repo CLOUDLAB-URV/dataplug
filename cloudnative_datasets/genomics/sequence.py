b5adc7e61f15ebb3a3bac1fbe44758ce563224ce
Aitor Arjona <aitor.a98@gmail.com>
2022-08-31 15:44:27 +0200

    Add FASTA empty

--- cloudnative_datasets/genomics/sequence.py
+++ cloudnative_datasets/genomics/sequence.py
@@ -1,6 +1,14 @@
+from typing import BinaryIO, Tuple, Dict
+
 from ..compressed.gzipped import GZippedText
+from ..cobase import CloudObjectBase
 
 
 class FASTQGZip(GZippedText):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+class FASTA(CloudObjectBase):
+    def preprocess(self, object_stream: BinaryIO) -> Tuple[bytes, Dict[str, str]]:
+        pass
