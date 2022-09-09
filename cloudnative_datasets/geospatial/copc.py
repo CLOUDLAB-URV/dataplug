2b9b8989585d97dea6e0e8c5fe775a32c47ffaeb
Aitor Arjona <aitor.a98@gmail.com>
2022-09-09 15:58:48 +0200

    LAS to COPC preprocessing

--- cloudnative_datasets/geospatial/copc.py
+++ cloudnative_datasets/geospatial/copc.py
@@ -1,6 +1,14 @@
-from ..cobase import CloudObjectWrapper
+import json
 
+import pdal
+import laspy.copc as lasp
+import json
+import tempfile
+import shutil
+
+from ..cobase import CloudObjectWrapper
 from ..preprocessers import BatchPreprocesser
+from ..util import force_delete_path
 
 
 class LiDARPreprocesser(BatchPreprocesser):
@@ -9,7 +17,55 @@ class LiDARPreprocesser(BatchPreprocesser):
 
     @staticmethod
     def preprocess(data_stream, meta):
-        super().preprocess(data_stream, meta)
+        input_file_path = tempfile.mktemp()
+        output_file_path = tempfile.mktemp()
+
+        try:
+            force_delete_path(input_file_path)
+            force_delete_path(output_file_path)
+
+            with open(input_file_path, 'wb') as input_file:
+                shutil.copyfileobj(data_stream, input_file)
+
+            pipeline_json = [
+                {
+                    "type": "readers.las",
+                    "filename": input_file_path
+                },
+                {
+                    "type": "writers.copc",
+                    "filename": output_file_path
+                }
+            ]
+
+            pipeline = pdal.Pipeline(json.dumps(pipeline_json))
+            pipeline.execute()
+
+            with open(output_file_path, 'rb') as copc_file:
+                copc_reader = lasp.CopcReader(copc_file)
+                copc_meta = {
+                    'points': str(copc_reader.header.point_count),
+                    'x_scale': str(copc_reader.header.x_scale),
+                    'y_scale': str(copc_reader.header.y_scale),
+                    'z_scale': str(copc_reader.header.z_scale),
+                    'x_offset': str(copc_reader.header.x_offset),
+                    'y_offset': str(copc_reader.header.y_offset),
+                    'z_offset': str(copc_reader.header.z_offset),
+                    'x_max': str(copc_reader.header.x_max),
+                    'y_max': str(copc_reader.header.y_max),
+                    'z_max': str(copc_reader.header.z_max),
+                    'x_min': str(copc_reader.header.x_min),
+                    'y_min': str(copc_reader.header.y_min),
+                    'z_min': str(copc_reader.header.z_min),
+                    'root_offset': str(copc_reader.copc_info.hierarchy_root_offset),
+                    'root_size': str(copc_reader.copc_info.hierarchy_root_size)
+                }
+
+                return open(output_file_path, 'rb'), copc_meta
+        finally:
+            force_delete_path(input_file_path)
+            force_delete_path(output_file_path)
+
 
 
 @CloudObjectWrapper(preprocesser=LiDARPreprocesser)
--- cloudnative_datasets/util.py
+++ cloudnative_datasets/util.py
@@ -1,4 +1,6 @@
 import re
+import os
+import shutil
 
 S3_PATH_REGEX = re.compile(r'^\w+://.+/.+$')
 
@@ -9,3 +11,11 @@ def split_s3_path(path):
 
     bucket, key = path.replace('s3://', '').split('/', 1)
     return bucket, key
+
+
+def force_delete_path(path):
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            os.remove(path)
+        elif os.path.isdir(path):
+            shutil.rmtree(path)
