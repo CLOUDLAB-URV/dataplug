7ce9ef34d173618f13c97ccab946574e73003139
Aitor Arjona <aitor.a98@gmail.com>
2022-11-09 22:53:18 +0100

    Update count words example, integration with lithops

--- examples/text_example.py
+++ examples/text_example.py
@@ -1,27 +1,53 @@
 import logging
+import time
+
+import botocore.config
+import lithops
+from collections import defaultdict
 
 from cloudnative_datasets import CloudObject
 from cloudnative_datasets.basic import UTF8Text, whole_words_strategy
 
+
 # logging.basicConfig(level=logging.DEBUG)
 # logging.getLogger("botocore").setLevel(logging.WARNING)
 # logging.getLogger('cloudnative_datasets').setLevel(logging.DEBUG)
+
+def count_words(data_slice):
+    t0 = time.perf_counter()
+    text = data_slice.get()
+    t1 = time.perf_counter()
+    print(f'Slice get: {t1-t0:.3f} s')
+
+    words = defaultdict(lambda: 0)
+    for word in text.split():
+        word = word.strip().lower().replace('.', '').replace(',', '')
+        words[word] += 1
+    return dict(words)
+
 
 if __name__ == '__main__':
     config = {
         'aws_access_key_id': 'minioadmin',
         'aws_secret_access_key': 'minioadmin',
         'region_name': 'us-east-1',
-        'endpoint_url': 'http://127.0.0.1:9000',
-        's3_config_kwargs': {
-            'signature_version': 's3v4'
-        }
+        'endpoint_url': 'http://192.168.1.110:9000',
+        'config': botocore.config.Config(signature_version='s3v4')
     }
 
     co = CloudObject.new_from_s3(UTF8Text, 's3://testdata/lorem_ipsum.txt', s3_config=config)
 
-    data_slices = co.partition(whole_words_strategy, num_chunks=100)
-    for data_slice in data_slices:
-        text = data_slice.get()
-        print(text)
-        print('---')
+    data_slices = co.partition(whole_words_strategy, num_chunks=8)
+
+    fexec = lithops.FunctionExecutor()
+    fexec.map(count_words, data_slices)
+    result = fexec.get_result()
+
+    merged = defaultdict(lambda: 0)
+    for map_result in result:
+        for word, count in map_result.items():
+            merged[word] += count
+
+    top = sorted(list(merged.items()), key=lambda tup: tup[1], reverse=True)
+
+    print(top[:10])
