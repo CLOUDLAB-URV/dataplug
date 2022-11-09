cc5a0f44e2eb411a225b28b3973dfdbc59b3b423
Aitor Arjona <aitor.a98@gmail.com>
2022-11-09 22:53:02 +0100

    Implement picklable s3 client

--- cloudnative_datasets/cobase.py
+++ cloudnative_datasets/cobase.py
@@ -1,15 +1,24 @@
+from __future__ import annotations
+
 import inspect
 import logging
-import functools
 import math
-from typing import Union
+from typing import Union, Callable, List, Concatenate
 from dataclasses import dataclass
-from typing import Tuple, Dict, BinaryIO
+from typing import TYPE_CHECKING, Tuple, Dict, Optional
 
 import boto3
 import botocore
 
-from .util import split_s3_path, PureS3Path, head_object
+from .cochunkbase import CloudObjectSlice
+
+if TYPE_CHECKING:
+    from mypy_boto3_s3 import S3Client
+else:
+    S3Client = object
+
+from .util import split_s3_path, head_object
+from .storage import PureS3Path, PickleableS3ClientProxy
 from .preprocessers import BatchPreprocesser, MapReducePreprocesser
 
 logger = logging.getLogger(__name__)
@@ -24,11 +33,12 @@ class CloudObjectWrapper:
         self.__preprocesser: Union[BatchPreprocesser, MapReducePreprocesser] = preprocesser
         self.__parent: 'CloudObjectWrapper' = inherit_from
 
-    def _get_preprocesser(self) -> Union[BatchPreprocesser, MapReducePreprocesser]:
+    @property
+    def preprocesser(self) -> Union[BatchPreprocesser, MapReducePreprocesser]:
         if self.__preprocesser is not None:
             return self.__preprocesser
         elif self.__parent is not None:
-            return self.__parent._get_preprocesser()
+            return self.__parent.preprocesser
         else:
             raise Exception('There is not preprocesser')
 
@@ -49,19 +59,18 @@ class CloudObject:
                  cloud_object_class: CloudObjectWrapper,
                  s3_uri_path: str,
                  s3_config: dict = None):
-        self._obj_meta = None
-        self._meta_meta = None
-        self._obj_path = PureS3Path.from_uri(s3_uri_path)
-        self._meta_path = PureS3Path.from_bucket_key(self._obj_path.bucket + '.meta', self._obj_path.key)
-        self._cls = cloud_object_class
-        self._s3_config = s3_config or {}
-
-        self._s3 = boto3.client('s3',
-                                aws_access_key_id=self._s3_config.get('aws_access_key_id'),
-                                aws_secret_access_key=self._s3_config.get('aws_secret_access_key'),
-                                region_name=self._s3_config.get('region_name'),
-                                endpoint_url=self._s3_config.get('endpoint_url'),
-                                config=botocore.client.Config(**self._s3_config.get('s3_config_kwargs', {})))
+        self._obj_meta: Optional[Dict[str, str]] = None
+        self._meta_meta: Optional[Dict[str, str]] = None
+        self._obj_path: PureS3Path = PureS3Path.from_uri(s3_uri_path)
+        self._meta_path: PureS3Path = PureS3Path.from_bucket_key(self._obj_path.bucket + '.meta', self._obj_path.key)
+        self._cls: CloudObjectWrapper = cloud_object_class
+        self._s3: PickleableS3ClientProxy = PickleableS3ClientProxy(
+            aws_access_key_id=s3_config.get('aws_access_key_id'),
+            aws_secret_access_key=s3_config.get('aws_secret_access_key'),
+            region_name=s3_config.get('region_name'),
+            endpoint_url=s3_config.get('endpoint_url'),
+            config=s3_config.get('s3_config_kwargs')
+        )
 
         logger.debug(f'{self._obj_path=},{self._meta_path=}')
 
@@ -70,24 +79,28 @@ class CloudObject:
         return self._obj_path
 
     @property
+    def meta_path(self) -> PureS3Path:
+        return self._meta_path
+
+    @property
     def size(self) -> int:
         if not self._obj_meta:
             self.fetch()
-        return self._obj_meta['ContentLength']
+        return int(self._obj_meta['ContentLength'])
 
     @property
-    def s3client(self):
+    def s3client(self) -> S3Client:
         return self._s3
 
     @classmethod
-    def new_from_s3(cls, cloud_object_class, s3_path, s3_config=None, fetch=True):
+    def new_from_s3(cls, cloud_object_class, s3_path, s3_config=None, fetch=True) -> 'CloudObject':
         co_instance = cls(cloud_object_class, s3_path, s3_config)
         if fetch:
             co_instance.fetch(enforce_obj=True)
         return co_instance
 
     @classmethod
-    def new_from_file(cls, cloud_object_class, file_path, cloud_path, s3_config=None):
+    def new_from_file(cls, cloud_object_class, file_path, cloud_path, s3_config=None) -> 'CloudObject':
         co_instance = cls(cloud_object_class, cloud_path, s3_config)
 
         if co_instance.exists():
@@ -96,6 +109,7 @@ class CloudObject:
         bucket, key = split_s3_path(cloud_path)
 
         co_instance._s3.upload_file(Filename=file_path, Bucket=bucket, Key=key)
+        return co_instance
 
     def exists(self) -> bool:
         if not self._obj_meta:
@@ -113,7 +127,8 @@ class CloudObject:
             else:
                 raise e
 
-    def fetch(self, enforce_obj: bool = False, enforce_meta: bool = False) -> Tuple[Dict[str, str], Dict[str, str]]:
+    def fetch(self, enforce_obj: bool = False, enforce_meta: bool = False) -> \
+            Tuple[Optional[Dict[str, str]], Optional[Dict[str, str]]]:
         """
         Get object metadata from storage with HEAD object request
         :param enforce_obj: True to raise KeyError exception if object key is not found in storage
@@ -145,7 +160,7 @@ class CloudObject:
             raise NotImplementedError()
 
     def __local_preprocess(self, chunk_size: int = None, num_workers: int = None):
-        preprocesser = self._cls._get_preprocesser()
+        preprocesser = self._cls.preprocesser
         if issubclass(preprocesser, BatchPreprocesser):
             get_res = self._s3.get_object(Bucket=self._obj_path.bucket, Key=self._obj_path.key)
             logger.debug(get_res)
@@ -171,7 +186,7 @@ class CloudObject:
             if body is None or meta is None:
                 raise Exception('Preprocessing result is {}'.format((body, meta)))
 
-            put_res = self._s3.upload_fileobj(
+            self._s3.upload_fileobj(
                 Fileobj=body,
                 Bucket=self._meta_bucket,
                 Key=self._meta_key,
@@ -181,7 +196,6 @@ class CloudObject:
             if hasattr(body, 'close'):
                 body.close()
 
-            logger.debug(put_res)
             self._obj_attrs.update(meta)
         elif issubclass(preprocesser, MapReducePreprocesser):
             head_res = self._s3.head_object(Bucket=self._obj_bucket, Key=self._obj_key)
@@ -239,9 +253,9 @@ class CloudObject:
             attr = getattr(self._child, func_name)
             return attr.__call__(*args, **kwargs)
 
-    def partition(self, strategy, *args, **kwargs):
+    def partition(self, strategy: Callable[Concatenate['CloudObject', ...], List[CloudObjectSlice]], *args, **kwargs):
         slices = strategy(self, *args, **kwargs)
-        [slice._populate(self) for slice in slices]
+        [slice.contextualize(self) for slice in slices]
         return slices
 
 
--- cloudnative_datasets/util.py
+++ cloudnative_datasets/util.py
@@ -1,11 +1,15 @@
+import json
 import logging
 import re
 import os
 import shutil
 from contextlib import suppress
 from pathlib import PurePath, _PosixFlavour
+from typing import Optional
 
+import boto3
 import botocore
+from mypy_boto3_s3 import S3Client
 
 logger = logging.getLogger(__name__)
 
@@ -46,97 +50,3 @@ def head_object(s3client, bucket, key):
     return response, metadata
 
 
-class _S3Flavour(_PosixFlavour):
-    is_supported = True
-
-    def parse_parts(self, parts):
-        drv, root, parsed = super().parse_parts(parts)
-        for part in parsed[1:]:
-            if part == '..':
-                index = parsed.index(part)
-                parsed.pop(index - 1)
-                parsed.remove(part)
-        return drv, root, parsed
-
-    def make_uri(self, path):
-        uri = super().make_uri(path)
-        return uri.replace('file:///', 's3://')
-
-
-class PureS3Path(PurePath):
-    """
-    PurePath subclass for AWS S3 service.
-    Source: https://github.com/liormizr/s3path
-    S3 is not a file-system but we can look at it like a POSIX system.
-    """
-    _flavour = _S3Flavour()
-    __slots__ = ()
-
-    @classmethod
-    def from_uri(cls, uri: str) -> 'PureS3Path':
-        """
-        from_uri class method create a class instance from url
-
-        >> from s3path import PureS3Path
-        >> PureS3Path.from_url('s3://<bucket>/<key>')
-        << PureS3Path('/<bucket>/<key>')
-        """
-        if not uri.startswith('s3://'):
-            raise ValueError('Provided uri seems to be no S3 URI!')
-        return cls(uri[4:])
-
-    @classmethod
-    def from_bucket_key(cls, bucket: str, key: str) -> 'PureS3Path':
-        """
-        from_bucket_key class method create a class instance from bucket, key pair's
-
-        >> from s3path import PureS3Path
-        >> PureS3Path.from_bucket_key(bucket='<bucket>', key='<key>')
-        << PureS3Path('/<bucket>/<key>')
-        """
-        bucket = cls(cls._flavour.sep, bucket)
-        if len(bucket.parts) != 2:
-            raise ValueError('bucket argument contains more then one path element: {}'.format(bucket))
-        key = cls(key)
-        if key.is_absolute():
-            key = key.relative_to('/')
-        return bucket / key
-
-    @property
-    def bucket(self) -> str:
-        """
-        The AWS S3 Bucket name, or ''
-        """
-        self._absolute_path_validation()
-        with suppress(ValueError):
-            _, bucket, *_ = self.parts
-            return bucket
-        return ''
-
-    @property
-    def key(self) -> str:
-        """
-        The AWS S3 Key name, or ''
-        """
-        self._absolute_path_validation()
-        key = self._flavour.sep.join(self.parts[2:])
-        return key
-
-    @property
-    def virtual_directory(self) -> str:
-        """
-        The parent virtual directory of a key
-        Example: foo/bar/baz -> foo/baz
-        """
-        vdir, _ = self.key.rsplit('/', 1)
-        return vdir
-
-    def as_uri(self) -> str:
-        """
-        Return the path as a 's3' URI.
-        """
-        return super().as_uri()
-
-    def _absolute_path_validation(self):
-        if not self.is_absolute():
-            raise ValueError('relative path have no bucket, key specification')
