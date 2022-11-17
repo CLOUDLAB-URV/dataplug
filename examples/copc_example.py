6b1892a2a620e7e005601aa2c7d50dd384602475
Aitor Arjona <aitor.a98@gmail.com>
2022-11-17 14:14:38 +0100

    Update examples

--- examples/copc_example.py
+++ examples/copc_example.py
@@ -8,13 +8,15 @@ from dataplug.util import setup_logging
 setup_logging(logging.INFO)
 
 if __name__ == '__main__':
-    config = {
+    # Localhost minio config
+    local_minio = {
         'aws_access_key_id': 'minioadmin',
         'aws_secret_access_key': 'minioadmin',
         'region_name': 'us-east-1',
         'endpoint_url': 'http://192.168.1.110:9000',
-        'config': {'signature_version': 's3v4'},
-        'use_token': False
+        'botocore_config_kwargs': {'signature_version': 's3v4'},
+        'role_arn': 'arn:aws:iam::123456789012:role/S3Access',
+        'use_token': True
     }
 
     co = CloudObject.from_s3(CloudOptimizedPointCloud,
